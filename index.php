c45a5d7f585a83dafa1ad3ee93345d487e4d1fca
bodzee94 <dbodden94@gmail.com>
2017-03-11 18:15:14 -0500

    converted navigation to includes

--- index.php
+++ index.php
@@ -12,63 +12,9 @@
 </head>
 
 <body>
-  <nav class="navbar navbar-default navbar-fixed-top navbar-custom">
-  <h2 class="hide">Main Navigation</h2>
-    <div class="container-fluid">
-
-      <!-- mobile navigation -->
-      <div class="navbar-header page-scroll">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-          <span class="sr-only">Toggle navigation menu</span>
-              Menu<i class="fa fa-bars"></i>
-        </button>
-          <a class="navbar-brand" href="index.php"><img src="images/MHS_logo.png" alt="Marine Heritage Society Logo" id="MHSlogo"></a><!--swap out for img tag for logo-->
-      </div>
-
-      <!-- desktop/tablet navigation -->
-      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav navbar-right">
-          <li>
-            <a href="index.php">Home</a>
-          </li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" id="dropdownMenu1" data-toggle="dropdown">About</a>
-              <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                <li>
-                  <a href="about.php">About</a>
-                </li>
-                <li>
-                  <a href="news.php">News</a>
-                </li>
-                <li>
-                  <a href="events.php">Events</a>
-                </li>
-                <li>
-                  <a href="restoration.php">Restoration</a>
-                </li>
-              </ul>
-          </li>
-          <li>
-            <a href="bookings.php">Book A Tour</a>
-          </li>
-            <li>
-            <a href="contact.php">Contact</a>
-          </li>
-          <li>
-            <a href="gallery.php">Gallery</a>
-          </li>
-
-          <li>
-            <a href="donations.php">Donations</a>
-          </li>
-          <li>
-            <a href="#"><img src="images/login.svg" alt="Admin Login Button" id="loginIcon"></a>
-          </li>
-        </ul>
-      </div>
-
-    </div><!-- closing nav containers -->
-  </nav>
+  <?php
+    include 'nav.php';
+   ?>
 
   <!--Marine Heritage Society Section-->
   <section class="row top-container">
