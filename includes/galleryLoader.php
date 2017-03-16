b9d0d72b3309973ee952f2bf0a13720ccd8bcb8f
bodzee94 <dbodden94@gmail.com>
2017-03-16 19:02:03 -0400

    made proper ajax query and added iquery tooperate gallery.

--- includes/galleryLoader.php
+++ includes/galleryLoader.php
@@ -1,20 +1,17 @@
 <?php
-	include ('connect.php');
+	include 'connect.php';
 
 	$mysqli = new mysqli($config['mysql_server'], $config['mysql_user'], $config['mysql_password'], $config['mysql_db']);
+	$mysqli->set_charset("utf8");
+
+	$query = "SELECT * FROM tbl_gallery WHERE gallery_title = {$title} AND gallery_thumb = {$thumb} AND gallery_full = {$full} AND gallery_photographer = {$credit}";
+
+	$result = mysqli_query($mysqli, $query);
 
 	if ($mysqli->connect_errno) {
 		printf("Connection failed: %s \n", $mysqli->connect_error);
 		exit();
 	}
-
-	$mysqli->set_charset("utf8");
-
-	$gallery = $_GET["gall"];
-
-	$query = "SELECT * FROM tbl_gallery WHERE gallery_title='$gallery' AND gallery_full='$gallery' AND gallery_photographer ='$gallery'";
-
-	$result = mysqli_query($mysqli, $query);
 
 	echo json_encode(mysqli_fetch_assoc($result));
 ?>
--- js/main.js
+++ js/main.js
@@ -1,94 +1,36 @@
 // Main JavaScript Scripts
 (function() {
-  console.log("SEAF has fired");
+  console.log("SEAF fired!");
 
-  var galleryTitle = document.querySelector('.modal-title');
-  var galleryImage = document.querySelector('.gallery-img');
-  var photographer = document.querySelector('.photographer');
-  var galleryNext = document.querySelector('.gallery-btn-next');
-  var galleryPrev = document.querySelector('.gallery-btn-prev');
-  var thumb = document.querySelectorAll('.thumb');
-  var httpRequest = new XMLHttpRequest();
+  $('.gallery-btn-next').on('click', function() {
+    $.ajax({
+      url: "includes/galleryLoader.php",
+      data: {title: this.id},
+      type: "GET"
+    })
 
-	var imgArray = new Array();
+    .done(function(galldata) {
+      console.log(galldata);
 
-  imgArray[0] = new Image();
-  imgArray[0].src = 'images/full1.jpg';
+      if (galldata !=="null") { //if there's a result and it's not null, do this...
+        data = JSON.parse(galldata);
 
-
-  imgArray[1] = new Image();
-  imgArray[1].src = 'images/full2.jpg';
-
-  imgArray[2] = new Image();
-  imgArray[2].src = 'images/full3.jpg';
-
-  imgArray[3] = new Image();
-  imgArray[3].src = 'images/full4.jpg';
-
-  imgArray[4] = new Image();
-  imgArray[4].src = 'images/full5.jpg';
-
-  imgArray[5] = new Image();
-  imgArray[5].src = 'images/full6.jpg';
-
-  imgArray[6] = new Image();
-  imgArray[6].src = 'images/full7.jpg';
-
-  imgArray[7] = new Image();
-  imgArray[7].src = 'images/full8.jpg';
-
-
-  function nextImage(next) {
-	//console.log("next");
-    var img = galleryImage;
-
-    for(var i = 0; i < imgArray.length;i++)
-    {
-        if(imgArray[i].src == img.src) // << check this
-        {
-            if(i == imgArray.length-1){
-               fullImg.src = imgArray[0].src;
-                break;
-            }
-           fullImg.src = imgArray[i+1].src;
-            break;
-        }
+        showresults(galldata);
+      }else{ //if there's no data being picked up, do this...
+        console.log('no data was retrieved!');
       }
-    }
+    })
 
-
-	for(var i = 0; i < thumb.length; i++){
-	galleryNext.addEventListener('click', nextImage, false);
-	}
-
-  function ajaxRequest() {
-    if(!httpRequest) {
-      console.log('Ajax did not work correctly');
-      return false;
-    }
-
-    var link = '../includes/galleryLoader.php' + '?gall=' + this.id;
-    httpRequest.onreadystatechange = showResults;
-    httpRequest.open('GET', link);
-    httpRequest.send();
-  }
-
-  function showResults() {
-    if(httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200)
-      //console.log(httpRequest.status);
-      console.log(httpRequest.readyState);
-
-      var galleryData = JSON.parse(httpRequest.responseText);
-
-      galleryTitle.firstChild.nodevalue = galleryData.gallery_image;
-
-      modalTitle.innerHTML = galleryData.gallery_title;
-      galleryImage.src = '../images/' + gallerylData.gallery_full + '.jpg';
-      photographer.innerHTML = galleryData.gallery_photographer;
-  }
-
-  [].forEach.call(thumb, function(img){
-    img.addEventListener('click', ajaxRequest, false);
+    .fail(function(ajaxCall, status, error) {
+      console.log(status, "," error);
+      console.dir(ajaxCall); //check the directory to make sure everything is connected
+    });
   });
 
+  function showResults(load) {
+    $('.modal-title').text(load.gallery_title);
+    $('.gallery-img').image(load.gallery_full);
+    $('.photographer').text(load.gallery_photographer);
+  }
+  
 })();
