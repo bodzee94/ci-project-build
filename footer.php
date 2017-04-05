8a5e36711458d56ca488f9a5e8db314dc9786c1b
bodzee94 <dbodden94@gmail.com>
2017-04-05 19:20:52 -0400

    removed script tag duplicates to fix issues with js

--- footer.php
+++ footer.php
@@ -138,9 +138,3 @@
       </div>
     </div><!--end of copyright footer-->
   </footer>
-
-<script src="js/main.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="js/bootstrap/bootstrap.min.js"></script>
-</body>
-</html>
